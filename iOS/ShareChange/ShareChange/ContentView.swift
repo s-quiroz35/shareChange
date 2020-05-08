c1e42ecad59f1912c9c00f374c57b1b93475126c
Rahil Patel <rahilpatel99@gmail.com>
2020-05-08 15:03:43 -0400

    init commit for app

---
+++ iOS/ShareChange/ShareChange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareChange
+//
+//  Created by Rahil Patel on 5/8/20.
+//  Copyright © 2020 RahilPatel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
